65ee078d3094479e8b55014eb8366e37e1a86040
Roman <roman.boxes@gmail.com>
2026-01-30 19:55:32 +0100

    refractor the makePageForEpisodes function for multiple elements
    - create footer element, and put there the link to the tvmaze.com as requested
    - refractor the makePageForEpisodes function for multiple elements
    - episodeTitle now displays always in two lines, if it's too long display the part of the title. Substring method only displaysthe part of the title not changes the original one.

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ function setup() {
   const section = document.createElement('section');
   section.className = 'episode-list'; 
   main.appendChild(section);
+  const footer = document.createElement('footer');
+  footer.className = 'page-footer';
+  footer.innerHTML = 'The data has (originally) come from <a href="https://tvmaze.com/">TVMaze.com</a>';
+  footer.style.textAlign = 'center';
+  footer.style.color = 'gray';
+  main.appendChild(footer);
+
 
   makePageForEpisodes(allEpisodes);
 }
@@ -27,17 +34,24 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   const section = document.querySelector('.episode-list');
-
-  const title_episode = document.createElement('h3');
-  title_episode.className = 'fi-title';
-  title_episode.textContent = `${episodeList[0].name} ${SxxExx(episodeList[0].season, episodeList[0].number)}`;
-  section.appendChild(title_episode);
-  const img = document.createElement('img');
-  img.src = episodeList[0].image.medium;
-  section.appendChild(img);
-  const summary = document.createElement('div');
-  summary.innerHTML = episodeList[0].summary;
-  section.appendChild(summary);
+  console.log(episodeList)
+  for (let episode of episodeList) {
+    const divider = document.createElement('div');
+    divider.className = 'divider';
+    section.appendChild(divider);
+    const title_episode = document.createElement('h3');
+    title_episode.className = 'fi-title';
+    const episodeTitle = episode.name.length < 25 ? episode.name : `${episode.name.substring(0, 22)}...`;
+    title_episode.textContent = `${episodeTitle}\n${SxxExx(episode.season, episode.number)}`;
+    divider.appendChild(title_episode);
+    const img = document.createElement('img');
+    img.src = episode.image.medium;
+    divider.appendChild(img);
+    const summary = document.createElement('div');
+    summary.innerHTML = episode.summary;
+    summary.style.color = 'white';
+    divider.appendChild(summary);
+  }
 }
 
 window.onload = setup;
