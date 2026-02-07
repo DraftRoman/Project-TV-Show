3d592b918b6060bf109d29e735f8ffd2ff3708fc
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-02-07 12:38:30 +0100

    Added feature of live searching for episode

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
-
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
@@ -23,7 +17,6 @@ function makePageForEpisodes(episodeList) {
   }
 }
 
-window.onload = setup;
 function formatEpisodeNumber(season,number) {
   return `\nS${season < 10 ? "0" : ""}${season}E${number < 10 ? "0" : ""}${number}`;
 }
@@ -31,3 +24,39 @@ function formatEpisodeName(name) {
   const maxLength = 25;
   return name.length < maxLength ? name : `${name.substring(0, maxLength - 3)}...`;
 }
+
+function filterEpisodes(episodeList,input){
+  document.querySelector(".episode-list").innerHTML =`<template id="episode-template">
+          <div class="divider">
+            <h3 class="fi-title"></h3>
+            <img class="episode-image" src="" alt="Episode Image" />
+            <div class="fi-summary"></div>
+          </div>
+        </template>`;
+    for (let episode of episodeList) {
+        const {name, season, number, summary} = episode;
+        const {medium} = episode.image;
+        const newCard = document.querySelector('#episode-template').content.cloneNode(true);
+        const newdiv = document.createElement("div");
+        if((name.includes(input)) || (summary.includes(input))){
+          newCard.querySelector(".fi-title").textContent = `${formatEpisodeName(name)} ${formatEpisodeNumber(season,number)}`;
+          newCard.querySelector("img").src = medium;
+          newCard.querySelector(".fi-summary").innerHtml = summary;
+          newdiv.append(newCard);
+        }
+
+      document.querySelector(".episode-list").append(newdiv);
+  }
+}
+
+function setup() {
+  const allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes); 
+
+  const inputValue = document.querySelector(".ep-search-input");
+  inputValue.addEventListener("keydown", () => {
+    filterEpisodes(allEpisodes,inputValue.value);
+  })
+
+}
+window.onload = setup;
